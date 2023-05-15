c58cd08c26140b9a67201f101b904f7249f8b535
dhf0527 <rbals0527@naver.com>
2023-05-15 19:59:24 +0900

    이동 및 애니메이션
    대쉬, 스프라이트 y축대칭 수정필요

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -9,44 +9,99 @@ public class Player : MonoBehaviour
         left,
         right
     }
-
     PlayerDir playerDir = PlayerDir.right;
 
-    [SerializeField] private Rigidbody2D rigid;
+    Rigidbody2D rigid;
+    Animator animator;
 
+    int dashCount = 0;
+    int maxDashCount = 2;
     float speed = 7f;
     float dashSpeed = 30f;
-    float jumpPower = 9f;
     float dashDis = 1.5f;
+    float dashDelay = 0;
+    float dashCoolTime = 1.2f;
+    float jumpPower = 9f;
     bool isGround = true;
     bool jumped = false;
+    bool isDashCool = false;
 
 
     // Start is called before the first frame update
     void Start()
     {
-
+        animator = GetComponent<Animator>();
+        rigid = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
         Move();
+        LookDir();
+        JumpAnimation();
         if (Input.GetKeyDown(KeyCode.C))
             Jump();
 
-        if (Input.GetKeyDown(KeyCode.Z))
-            StartCoroutine("Dash");
+        if (Input.GetKeyDown(KeyCode.Z) && !isDashCool)
+        {
+            StartCoroutine("StartDash");
+        }
+    }
+
+    void LookDir()
+    {
+        switch (playerDir)
+        {
+            case PlayerDir.right:
+                transform.rotation = Quaternion.Euler(0, 0, 0);
+                break;
+
+            case PlayerDir.left:
+                transform.rotation = Quaternion.Euler(0, 180, 0);
+                break;
+        }
+    }
+
+    void JumpAnimation()
+    {
+        if (rigid.velocity.y > 0.05f)
+        {
+            animator.SetBool("Jump", true);
+            animator.SetBool("Fall", false);
+        }
+        else if (rigid.velocity.y < -0.05f)
+        {
+            animator.SetBool("Fall", true);
+            animator.SetBool("Jump", false);
+        }
+        else
+        {
+            animator.SetBool("Jump", false);
+            animator.SetBool("Fall", false);
+        }
     }
 
     void Move()
     {
         float x = Input.GetAxisRaw("Horizontal");
-        transform.Translate(new Vector2(x * Time.deltaTime * speed, 0));
+        transform.Translate(transform.right * (x * Time.deltaTime * speed));
+        // transform.Translate(new Vector2(x * Time.deltaTime * speed, 0));
+
+        animator.SetBool("Walk", true);
         if (x > 0)
+        {
             playerDir = PlayerDir.right;
+        }
         else if (x < 0)
+        {
             playerDir = PlayerDir.left;
+
+        }
+        else
+        {
+            animator.SetBool("Walk", false);
+        }
     }
 
     void Jump()
@@ -61,23 +116,58 @@ public class Player : MonoBehaviour
             }
         }
 
-        rigid.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
+        rigid.AddForce(transform.up * jumpPower, ForceMode2D.Impulse);
+    }
+
+    IEnumerator StartDash()
+    {
+        if (dashCount < maxDashCount)
+        {
+            StartCoroutine("Dash");
+            dashCount++;
+        }
+        
+        if(dashCount >= maxDashCount)
+        {
+            isDashCool = true;  
+            dashDelay = dashCoolTime;
+            while (dashDelay > 0)
+            {
+                dashDelay -= Time.deltaTime;
+                Debug.Log(dashDelay);
+                yield return new WaitForFixedUpdate();
+            }
+            dashCount = 0;
+            isDashCool = false;
+        } 
     }
 
     IEnumerator Dash()
     {
         float dashed = 0;
-        int x;
-        if (playerDir == PlayerDir.right)
-            x = 1;
-        else
-            x = -1;
+        int dir;
+        dir = playerDir == PlayerDir.right ? 1 : -1;
+
+        animator.SetBool("Dash", true);
 
         while (dashed < dashDis)
         {
-            float temp = Time.deltaTime * dashSpeed;
-            transform.Translate(new Vector2(x * temp, 0));
-            dashed += temp;
+            float deltaDash = Time.deltaTime * dashSpeed;
+            transform.Translate(transform.right * (dir * deltaDash));
+            dashed += deltaDash;
+            yield return new WaitForFixedUpdate();
+        }
+        
+        animator.SetBool("Dash", false);
+    }
+
+    IEnumerator CoolDown(float coolTime)
+    {
+        float delay = coolTime;
+
+        while (delay > 0)
+        {
+            delay -= Time.deltaTime;
             yield return new WaitForFixedUpdate();
         }
     }
