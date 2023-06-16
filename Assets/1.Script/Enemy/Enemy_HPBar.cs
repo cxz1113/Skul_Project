b774b7d7f3be0e7eda82bac097877e5f1fd53a7b
dhf0527 <rbals0527@naver.com>
2023-06-16 17:51:02 +0900

    적 hp바 구현 완료, 궁수(적) 작업중

--- Assets/1.Script/Enemy/Enemy_HPBar.cs
+++ Assets/1.Script/Enemy/Enemy_HPBar.cs
@@ -1,27 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Enemy_HPBar : MonoBehaviour
 {
     public GameObject prfHP;
     public GameObject canvas;
+    public Enemy enemy;
 
-    RectTransform hpBar;
+    RectTransform hpBar_Rect;
+    GameObject hpBar;
+    Image hpBar_img;
 
     public float height;
+    public float hpBar_width;
 
     // Start is called before the first frame update
     void Start()
     {
         canvas = GameObject.Find("InGameCanvas");
-        hpBar = Instantiate(prfHP, canvas.transform).GetComponent<RectTransform>();
+        hpBar_Rect = Instantiate(prfHP, canvas.transform).GetComponent<RectTransform>();
+        hpBar = hpBar_Rect.GetChild(0).gameObject;
+        hpBar_img = hpBar.transform.GetChild(0).GetComponent<Image>();
+        RectTransform hpBarBG_Rect = hpBar.GetComponent<RectTransform>();
+        hpBarBG_Rect.sizeDelta = new Vector2(hpBar_width, hpBarBG_Rect.sizeDelta.y);
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 hpBarPos = Camera.main.WorldToScreenPoint(transform.position);
-        hpBar.position = hpBarPos;
+        if (enemy.ed.hp == enemy.ed.maxhp)
+            hpBar.SetActive(false);
+        else
+        {
+            if (enemy.ed.hp <=0)
+            {
+                Destroy(hpBar);
+                return;
+            }
+
+            hpBar.SetActive(true);  
+            hpBar_img.fillAmount = enemy.ed.hp / enemy.ed.maxhp;
+            Vector3 hpBarPos = Camera.main.WorldToScreenPoint(transform.position - Vector3.up * height);
+            hpBar_Rect.anchoredPosition = hpBarPos;
+        }
     }
 }
